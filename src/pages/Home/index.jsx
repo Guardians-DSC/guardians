cf52c892fee03bad68c8ba9ac5b94f3ce2a62a72
vitor barbosa <vitorbarbosa789@gmail.com>
2022-11-02 01:58:35 -0300

    [ADD] home component + styles web

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,73 @@
-import { Wrapper, Text } from "./styles";
+import { Content, Wrapper, Title, Code, WrapperButtons, WrapperIcons, Blink} from "./styles";
+import { Trans, useTranslation } from 'react-i18next';
+import { AiOutlineInstagram, AiFillLinkedin, AiOutlineGithub } from "react-icons/ai";
+import { useNavigate} from 'react-router-dom';
+
 const Home = () => {
+
+    const { t } = useTranslation();
+
+    const navigate = useNavigate();
+
+    const navigateToAbout = () => {
+        navigate('/about')
+    }
+
+    const navigateToArticles = () => {
+        navigate('/articles')
+    }
+
     return (
-        <Wrapper>
-            <Text>
-                HEY, IT'S GUARDIANS@UFCG
-            </Text>
-        </Wrapper>
+        <Content>
+            <Wrapper>
+                <Code>{ t('home.upperText')}</Code>
+                <Title>
+                    <Trans i18nKey={ 'home.title'}/>
+                    <Blink>{ t('home.blinkBar')}</Blink>
+                </Title>
+
+                <WrapperButtons>
+                    <button
+                        onClick={navigateToAbout}
+                        style={{ 
+                            backgroundColor: "#0D548B", 
+                            borderRadius: "20px 0 0 0", 
+                            color: "white",
+                            width: "160px",
+                            height: "42px",
+                            border: "0"
+                        }}>
+                        { t('home.buttons.principal')}
+                    </button>
+                    <button
+                        onClick={navigateToArticles}
+                        style={{ 
+                            border: "3",
+                            borderColor: "#328FAC",
+                            color: "#328FAC",
+                            backgroundColor: "none",
+                            borderRadius: "0 0 20px 0", 
+                            width: "160px",
+                            height: "42px"
+                        }}
+                    >
+                        { t('home.buttons.secundary')}
+                    </button>
+                </WrapperButtons>
+
+                <WrapperIcons>
+                        <a href="https://www.instagram.com/guardiansufcg/" style={{ color: "#328FAC"}} target="_blank">
+                            <AiOutlineInstagram/>   
+                        </a>
+                        <a href="https://www.linkedin.com/company/guardians-ufcg" style={{ color: "#328FAC"}} target="_blank">
+                            <AiFillLinkedin/>  
+                        </a>
+                        <a href="https://github.com/Guardians-DSC" style={{ color: "#328FAC"}} target="_blank">
+                            <AiOutlineGithub/>
+                        </a>
+                </WrapperIcons>
+            </Wrapper>
+        </Content>
     )
 }
 export default Home;
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,15 +1,67 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.main`
+export const Content  = styled.main`
     width: 100%;
     height: 100vh;
     background-color: #E3E3E3;
     display: flex;
     justify-content: center;
     align-items: center;
+
+`
+export const Wrapper = styled.main`
+    width: 80vw;
+    display: flex;
+    flex-direction: column;
 `
 
-export const Text = styled.h1`
+export const Title = styled.h1`
     color: var(--dark-blue);
-    font-size: 2rem;
+    font-size: 48px;
+    display: flex;
+    align-items: flex-end;
+`
+
+export const Code = styled.h1`
+    color: var(--light-blue);
+    font-size: 12px;
+`
+
+export const WrapperButtons = styled.h1`
+    display: flex;
+    width: 26vw;
+    justify-content: space-between;
+    margin-top: 1vh;
+`
+
+export const Button = styled.h1`
+    color: white;
+    width: 182px;
+    height: 54px;
+`
+
+export const WrapperIcons = styled.h1`
+    display: flex;
+    width: 20vw;
+    margin-top: 1vh;
+    margin-right: 4vw;
+    color: var(--light-blue);
+    
+`
+
+export const Blink = styled.h1`
+    font-size: 50px;
+    color: var(--dark-blue);
+    position: absolute;
+    left: 31vw;
+
+    @keyframes blinker {
+        50% {
+        opacity: 0;
+        }
+    }
+
+    animation: blinker 1s linear infinite;
+
+
 `
